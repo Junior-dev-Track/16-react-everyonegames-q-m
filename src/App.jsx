345e062746bcf200e019ca6ce46bd603d1cddfcc
QuentinCirri <cirri.quentin@mail.com>
2024-04-25 09:53:40 +0200

    useeffect api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import API_KEY from "./apikey";
 
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://api.rawg.io/api/platforms?key=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
   return (
     <>
       <Router>
