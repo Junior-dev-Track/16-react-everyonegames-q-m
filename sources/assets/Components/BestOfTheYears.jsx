eb3359ffa02151e015ab6fab8ead5f2d4d3aa5b3
QuentinCirri <cirri.quentin@mail.com>
2024-05-07 11:55:15 +0200

    bestoftheyear

--- sources/assets/Components/BestOfTheYears.jsx
+++ sources/assets/Components/BestOfTheYears.jsx
@@ -17,17 +17,20 @@ const BestOfTheYears = () => {
   return (
     <main>
       <div>
-        <h1>coucou</h1>
         <section>
           {data &&
             data.results &&
-            data.results.map((game, index) => (
-              <div key={index}>
-                <h2>{game.name}</h2>
+            data.results
+              .sort((a, b) => b.rating - a.rating)
+              .map((game, index) => (
+                <div key={index}>
+                  <h2>{game.name}</h2>
+                  <p>Game Rating: {game.rating}</p>
+                  <img style={{ width: "100px" }} src={game.background_image} />
 
-                <p>{game.description}</p>
-              </div>
-            ))}
+                  <p>{game.description}</p>
+                </div>
+              ))}
         </section>
       </div>
     </main>
