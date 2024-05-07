26905b0696c1d3cce3a8d50eba3c90012635dd5b
QuentinCirri <cirri.quentin@mail.com>
2024-05-07 09:25:32 +0200

    link path

--- sources/assets/Components/Nav.jsx
+++ sources/assets/Components/Nav.jsx
@@ -10,52 +10,52 @@ const Nav = () => {
           </Link>
         </li>
         <li>
-          <Link to="platforms">
+          <Link to="/platforms">
             <button>Platforms</button>
           </Link>
         </li>
         <li>
-          <Link to="store">
+          <Link to="/store">
             <button>Stores</button>
           </Link>
         </li>
         <li>
-          <Link to="genre">
+          <Link to="/genre">
             <button>Genres</button>
           </Link>
         </li>
         <li>
-          <Link to="creators">
+          <Link to="/creators">
             <button>Creators</button>
           </Link>
         </li>
         <li>
-          <Link to="bestoftheyears">
+          <Link to="/bestoftheyears">
             <button>Best game of the year</button>
           </Link>
         </li>
         <li>
-          <Link to="newreleases">
+          <Link to="/newreleases">
             <button>New releases</button>
           </Link>
         </li>
         <li>
-          <Link to="nextweekreleases">
+          <Link to="/nextweekreleases">
             <button>Next week releases</button>
           </Link>
         </li>
         <li>
-          <Link to="tags">
+          <Link to="/tags">
             <button>Tags</button>
           </Link>
         </li>
         <li>
-          <Link to="developers">
+          <Link to="/developers">
             <button>Developers</button>
           </Link>
         </li>
         <li>
-          <Link to="publisher">
+          <Link to="/publisher">
             <button>Publisher</button>
           </Link>
         </li>
